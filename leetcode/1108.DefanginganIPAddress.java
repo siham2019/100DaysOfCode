7769a311e32750b132fbd951fd2bbe2420aa6940
Deepansh Singh <deepansh54@gmail.com>
2020-10-06 23:54:40 +0530

    Day 14 leetcode #100DaysOfCode

--- leetcode/1108.DefanginganIPAddress.java
+++ leetcode/1108.DefanginganIPAddress.java
@@ -1,6 +1,15 @@
 class Solution {
     public String defangIPaddr(String address) {
-        return address.replaceAll("\\.","[.]");
+        StringBuilder r = new StringBuilder();
+        for(int i=0;i<address.length();i++){
+            if(address.charAt(i) == '.'){
+                r.append("[.]");
+            }
+            else{
+                r.append(String.valueOf(address.charAt(i)));
+            }
+        }
+        return r.toString();
     }
 }
 //https://leetcode.com/problems/defanging-an-ip-address/
